cf7b7fa76f4606cf747e0dcc4490899bb50b1fd3
Kianeay <70838054+Kianeay@users.noreply.github.com>
2020-10-22 17:36:38 +0300

    feat: add background

--- momentum/script.js
+++ momentum/script.js
@@ -1,104 +1,81 @@
-// DOM Elements
-const time = document.querySelector('.time'),
-  greeting = document.querySelector('.greeting'),
-  name = document.querySelector('.name'),
-  focus = document.querySelector('.focus');
+const time = document.querySelector('.time');
+const focus = document.querySelector('.focus');
+const greeting = document.querySelector('.greeting');
+const name = document.querySelector('.name');
 
-// Options
-const showAmPm = true;
-
-// Show Time
 function showTime() {
-  let today = new Date(),
+    let today = new Date(),
     hour = today.getHours(),
     min = today.getMinutes(),
     sec = today.getSeconds();
 
-  // Set AM or PM
-  const amPm = hour >= 12 ? 'PM' : 'AM';
+//const amPm = hour >= 12 ? 'PM' : 'AM';
 
-  // 12hr Format
-  hour = hour % 12 || 12;
+//hour = hour % 12 || 12;
 
-  // Output Time
-  time.innerHTML = `${hour}<span>:</span>${addZero(min)}<span>:</span>${addZero(
-    sec
-  )} ${showAmPm ? amPm : ''}`;
+time.innerHTML = `${hour}<span>:</span>${addZero(min)}<span>:</span>${addZero(sec)}`;
 
-  setTimeout(showTime, 1000);
+setTimeout(showTime, 1000);
+
 }
 
-// Add Zeros
 function addZero(n) {
-  return (parseInt(n, 10) < 10 ? '0' : '') + n;
+    return (parseInt(n, 10) < 10 ? '0' : '') + n;
 }
 
-// Set Background and Greeting
 function setBgGreet() {
-  let today = new Date(),
+    let today = new Date(),
     hour = today.getHours();
 
-  if (hour < 12) {
-    // Morning
-    document.body.style.backgroundImage =
-      "url('https://i.ibb.co/7vDLJFb/morning.jpg')";
-    greeting.textContent = 'Good Morning, ';
-  } else if (hour < 18) {
-    // Afternoon
-    document.body.style.backgroundImage =
-      "url('https://i.ibb.co/3mThcXc/afternoon.jpg')";
-    greeting.textContent = 'Good Afternoon, ';
-  } else {
-    // Evening
-    document.body.style.backgroundImage =
-      "url('https://i.ibb.co/924T2Wv/night.jpg')";
-    greeting.textContent = 'Good Evening, ';
-    document.body.style.color = 'white';
-  }
+    if (hour < 12) {
+        document.body.style.backgroundImage = "url('https://i.ibb.co/7vDLJFb/morning.jpg')";
+        greeting.textContent = 'Good Morning';
+    } else if (hour < 18) {
+        document.body.style.backgroundImage = "url('https://i.ibb.co/3mThcXc/afternoon.jpg')";
+        greeting.textContent = 'Good Afternoon';       
+    } else {
+        document.body.style.backgroundImage = "url('https://i.ibb.co/924T2Wv/night.jpg')";
+        greeting.textContent = 'Good Evening';   
+        document.body.style.color = 'white';
+    }
 }
 
-// Get Name
 function getName() {
-  if (localStorage.getItem('name') === null) {
-    name.textContent = '[Enter Name]';
-  } else {
-    name.textContent = localStorage.getItem('name');
-  }
+    if (localStorage.getItem('name') === null) {
+        name.textContent = 'Enter Name';
+    } else {
+        name.textContent = localStorage.getItem('name');
+    }
 }
 
-// Set Name
 function setName(e) {
-  if (e.type === 'keypress') {
-    // Make sure enter is pressed
-    if (e.which == 13 || e.keyCode == 13) {
-      localStorage.setItem('name', e.target.innerText);
-      name.blur();
+    if (e.type === 'keypress') {
+      if (e.which == 13 || e.keyCode == 13) {
+        localStorage.setItem('name', e.target.innerText);
+        name.blur();
+      }
+    } else {
+        localStorage.setItem('name', e.target.innerText);
     }
-  } else {
-    localStorage.setItem('name', e.target.innerText);
-  }
 }
 
-// Get Focus
 function getFocus() {
-  if (localStorage.getItem('focus') === null) {
-    focus.textContent = '[Enter Focus]';
-  } else {
-    focus.textContent = localStorage.getItem('focus');
-  }
+    if (localStorage.getItem('focus') === null) {
+        focus.textContent = 'Enter Focus';
+    } else {
+        focus.textContent = localStorage.getItem('focus');
+    }
 }
 
-// Set Focus
 function setFocus(e) {
-  if (e.type === 'keypress') {
-    // Make sure enter is pressed
-    if (e.which == 13 || e.keyCode == 13) {
-      localStorage.setItem('focus', e.target.innerText);
-      focus.blur();
+    if (e.type === 'keypress') {
+      if (e.which == 13 || e.keyCode == 13) {
+        localStorage.setItem('focus', e.target.innerText);
+        focus.blur();
+      }
+    } else {
+        localStorage.setItem('focus', e.target.innerText);
     }
-  } else {
-    localStorage.setItem('focus', e.target.innerText);
-  }
 }
 
 name.addEventListener('keypress', setName);
@@ -111,3 +88,29 @@ showTime();
 setBgGreet();
 getName();
 getFocus();
+
+//
+
+const base = 'https://raw.githubusercontent.com/irinainina/ready-projects/momentum/momentum/assets/images/night/';
+const images = ['01.jpg', '02.jpg', '03.jpg', '05.jpg', '06.jpg', '07.jpg', '08.jpg', '09.jpg', '10.jpg', '11.jpg', '12.jpg', '13.jpg', '14.jpg', '15.jpg', '16.jpg', '17.jpg', '18.jpg', '19.jpg', '20.jpg'];
+let i = 0;
+
+function viewBgImage(data) {
+  const body = document.querySelector('body');
+  const src = data;
+  const img = document.createElement('img');
+  img.src = src;
+  img.onload = () => {      
+    body.style.backgroundImage = `url(${src})`;
+  }; 
+}
+function getImage() {
+  const index = i % images.length;
+  const imageSrc = base + images[index];
+  viewBgImage(imageSrc);
+  i++;
+  btn.disabled = true;
+  setTimeout(function() { btn.disabled = false }, 1000);
+} 
+const btn = document.querySelector('.btn');
+btn.addEventListener('click', getImage);
