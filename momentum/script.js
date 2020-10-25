45613315c0d6937654920b44202626b5c690e2dc
Kianeay <70838054+Kianeay@users.noreply.github.com>
2020-10-25 22:00:40 +0300

    feat: add quote

--- momentum/script.js
+++ momentum/script.js
@@ -7,21 +7,35 @@ const date = document.querySelector('.date');
 //weather
 const weatherIcon = document.querySelector('.weather-icon');
 const temperature = document.querySelector('.temperature');
+const air = document.querySelector('.air');
+const wind = document.querySelector('.wind');
 const weatherDescription = document.querySelector('.weather-description');
+const city = document.querySelector('.city');
 
 async function getWeather() {
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=Минск&lang=ru&appid=08f2a575dda978b9c539199e54df03b0&units=metric`;
-    const res = await fetch(url);
-    const data = await res.json();
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city.textContent}&lang=ru&appid=8e60446652d969eb705138337668aa6c&units=metric`;
+  const res = await fetch(url);
+  const data = await res.json();
   
-    weatherIcon.classList.add(`owf-${data.weather[0].id}`);
-    temperature.textContent = `${data.main.temp}°C`;
-    weatherDescription.textContent = data.weather[0].description;
+  
+  weatherIcon.className = 'weather-icon owf';
+  weatherIcon.classList.add(`owf-${data.weather[0].id}`);
+  temperature.textContent = `${data.main.temp.toFixed(0)}°C`;
+  air.textContent = `${data.main.humidity.toFixed(0)}%`;
+  wind.textContent = `${data.wind.speed.toFixed(0)}км/ч`;
+  weatherDescription.textContent = data.weather[0].description;
+}
+
+function setCity(event) {
+  if (event.code === 'Enter') {
+    getWeather();
+    city.blur();
   }
+}
 
+document.addEventListener('DOMContentLoaded', getWeather);
+city.addEventListener('keypress', setCity);
 
-
-  
 function showTime() {
     let today = new Date(),
     hour = today.getHours(),
@@ -62,17 +76,17 @@ function setBgGreet() {
     hour = today.getHours();
 
     if (hour < 12 && hour > 6) {
-        document.body.style.backgroundImage = "url('https://i.ibb.co/7vDLJFb/morning.jpg')";
+        document.body.style.backgroundImage = "url('assets/images/morning/12.jpg')";
         greeting.textContent = 'Доброе утро, ';
     } else if (hour < 18) {
-        document.body.style.backgroundImage = "url('https://i.ibb.co/3mThcXc/afternoon.jpg')";
+        document.body.style.backgroundImage = "url('assets/images/day/12.jpg')";
         greeting.textContent = 'Добрый день, ';       
     } else if (hour < 24) {
-        document.body.style.backgroundImage = "url('https://i.ibb.co/924T2Wv/night.jpg')";
+        document.body.style.backgroundImage = "url('assets/images/evening/03.jpg')";
         greeting.textContent = 'Добрый вечер, ';   
         document.body.style.color = 'white';
     } else {
-        document.body.style.backgroundImage = "url('https://i.ibb.co/924T2Wv/night.jpg')";
+        document.body.style.backgroundImage = "url('assets/images/night/14.jpg')";
         greeting.textContent = 'Доброй ночи, ';   
         document.body.style.color = 'white';
     }
@@ -131,8 +145,19 @@ showDate();
 
 //
 
-const base = 'https://raw.githubusercontent.com/irinainina/ready-projects/momentum/momentum/assets/images/night/';
-const images = ['01.jpg', '02.jpg', '03.jpg', '05.jpg', '06.jpg', '07.jpg', '08.jpg', '09.jpg', '10.jpg', '11.jpg', '12.jpg', '13.jpg', '14.jpg', '15.jpg', '16.jpg', '17.jpg', '18.jpg', '19.jpg', '20.jpg'];
+const baseDay = 'assets/images/day/';
+const imagesDay = ['01.jpg', '02.jpg', '03.jpg', '05.jpg', '06.jpg', '07.jpg', '08.jpg', '09.jpg', '10.jpg', '11.jpg', '12.jpg', '13.jpg', '14.jpg', '15.jpg', '16.jpg', '17.jpg', '18.jpg', '19.jpg', '20.jpg'];
+
+const baseEvening = 'assets/images/evening/';
+const imagesEvening = ['01.jpg', '02.jpg', '03.jpg', '05.jpg', '06.jpg', '07.jpg', '08.jpg', '09.jpg', '10.jpg', '11.jpg', '12.jpg', '13.jpg', '14.jpg', '15.jpg', '16.jpg', '17.jpg', '18.jpg', '19.jpg', '20.jpg'];
+
+const baseMor = 'assets/images/morning/';
+const imagesMor = ['01.jpg', '02.jpg', '03.jpg', '05.jpg', '06.jpg', '07.jpg', '08.jpg', '09.jpg', '10.jpg', '11.jpg', '12.jpg', '13.jpg', '14.jpg', '15.jpg', '16.jpg', '17.jpg', '18.jpg', '19.jpg', '20.jpg'];
+
+const baseNight = 'assets/images/night/';
+const imagesNight = ['01.jpg', '02.jpg', '03.jpg', '05.jpg', '06.jpg', '07.jpg', '08.jpg', '09.jpg', '10.jpg', '11.jpg', '12.jpg', '13.jpg', '14.jpg', '15.jpg', '16.jpg', '17.jpg', '18.jpg', '19.jpg', '20.jpg'];
+
+
 let i = 0;
 
 function viewBgImage(data) {
@@ -145,12 +170,57 @@ function viewBgImage(data) {
   }; 
 }
 function getImage() {
-  const index = i % images.length;
-  const imageSrc = base + images[index];
-  viewBgImage(imageSrc);
-  i++;
+    let today = new Date();
+    let hour = today.getHours();
+  const indexD = i % imagesDay.length;
+  const indexM = i % imagesMor.length;
+  const indexE = i % imagesEvening.length;
+  const indexN = i % imagesNight.length;
+
+  const imageSrcDay = baseDay + imagesDay[indexD];
+  const imageMor = baseMor + imagesMor[indexM];
+  const imageSrcE = baseEvening + imagesEvening[indexE];
+  const imageSrcN = baseNight + imagesNight[indexN];
+
+  if (hour < 12 && hour > 6) {
+    viewBgImage(imageMor);
+    i++;
   btn.disabled = true;
   setTimeout(function() { btn.disabled = false }, 1000);
+  } else if (hour < 18) {
+    viewBgImage(imageSrcDay);
+    i++;
+  btn.disabled = true;
+  setTimeout(function() { btn.disabled = false }, 1000);
+  } else if (hour < 24) {
+    viewBgImage(imageSrcE);
+    i++;
+  btn.disabled = true;
+  setTimeout(function() { btn.disabled = false }, 1000);
+  } else {
+    viewBgImage(imageSrcN);
+    i++;
+  btn.disabled = true;
+  setTimeout(function() { btn.disabled = false }, 1000);
+  }
+  
+  
 } 
 const btn = document.querySelector('.btn');
 btn.addEventListener('click', getImage);
+
+//quote
+
+const blockquote = document.querySelector('blockquote');
+const figcaption = document.querySelector('figcaption');
+
+
+
+async function getQuote() {  
+  const url = `https://cors-anywhere.herokuapp.com/https://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en;`;
+  const res = await fetch(url);
+  const data = await res.json(); 
+  blockquote.textContent = data.quoteText;
+  figcaption.textContent = data.quoteAuthor;
+}
+document.addEventListener('DOMContentLoaded', getQuote);
